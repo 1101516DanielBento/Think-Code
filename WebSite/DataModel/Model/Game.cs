3f42481083db3c294cdcf4ef863f956d0165dc61
d4rc92 <danibento619@gmail.com>
2013-12-18 15:56:29 +0000

    [ARQSI] Criacao / modificacao de classes de modelo de dados: Amizade, Pedido de amizade, Negociacoes, Jogo e Pedido de jogo.

---
+++ WebSite/DataModel/Model/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.Model
+{
+    public class Game
+    {
+        private int _idGame;
+
+        public int IdGame
+        {
+            get { return _idGame; }
+            set { _idGame = value; }
+        }
+
+        private string _gameName;
+
+        public string GameName
+        {
+            get { return _gameName; }
+            set { _gameName = value; }
+        }
+    }
+}
