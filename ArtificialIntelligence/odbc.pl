2472b6bfaa34aff7f16acc8eb23dd307ed2d31a3
Fred Almeida <1101008@isep.ipp.pt>
2013-12-19 00:20:43 +0000

    [ALGAV] Update: a base de conhecimento já sabe quem é o utilizador, e amigos, e respectivas tags.

--- ArtificialIntelligence/odbc.pl
+++ ArtificialIntelligence/odbc.pl
@@ -10,45 +10,74 @@ connect:-
 		     alias(wvm023),
 		     open(once)]).
 
-users(U):-
-	odbc_query(wvm023, 'SELECT username FROM dbo.[User]', U,[ types([default])]).
+allUsers(L):-
+	findall(X, odbc_query(wvm023, 'SELECT username FROM dbo.[User]', row(X)),L).
 
-toBaseKnowledge(Id):-userByID(Id,_).
 
-userByID(UId,User):-
+toBaseKnowledge(UId):-
+	usernameByID(UId,_).
+
+
+usernameByID(UId,Username):-
 	string_concat('SELECT username FROM dbo.[User] WHERE idUser=', (UId), Sql),
-	findall(X, odbc_query(wvm023, Sql , row(X)), [User|_]),
-	userKnowledge(User),
-	userTagIDs(UId, TagList),
-	userTagsKnowledge(User, TagList).
+	findall(X, odbc_query(wvm023, Sql , row(X)), [Username|_]),
+        knowUser(UId, Username),
+	knowFriends(UId).
 
 
 userTagIDs(UId, TagList):-
-	string_concat('SELECT idTag FROM dbo.[UserTags] WHERE idUser=', (UId), SqlTagID),
-	findall(X, odbc_query(wvm023, SqlTagID, row(X)), TagIDList),
+	string_concat('SELECT idTag FROM dbo.[UserTags] WHERE idUser=', (UId), Sql),
+	findall(X, odbc_query(wvm023, Sql, row(X)), TagIDList),
 	userTags(TagIDList, TagList).
 
 
-userTags([],[]).
+userTags([],[]):-!.
 userTags([H|T],[Tag|TagList]):-
-	string_concat('SELECT tagName FROM dbo.[TagList] WHERE idTag=', (H), SqlTags),
-	findall(X, odbc_query(wvm023, SqlTags, row(X)), [Tag|_]),
+	string_concat('SELECT tagName FROM dbo.[TagList] WHERE idTag=', (H), Sql),
+	findall(X, odbc_query(wvm023, Sql, row(X)), [Tag|_]),
 	userTags(T, TagList).
 
 
-friends(UId,IDs):-
+knowFriends(UId):-
 	string_concat('SELECT idUserB FROM dbo.[FriendShip] WHERE idUserA=', (UId), Sql1),
 	findall(X, odbc_query(wvm023, Sql1, row(X)), IDs1),
 	string_concat('SELECT idUserA FROM dbo.[FriendShip] WHERE idUserB=', (UId), Sql2),
 	findall(X, odbc_query(wvm023, Sql2, row(X)), IDs2),
-	append(IDs1,IDs2,IDs).
+	append(IDs1,IDs2,IDs),
+	userByIdList(IDs).
 
 
-userKnowledge(U):-
-	assertz(user(U)).
+userByIdList([]):-!.
+userByIdList([H|T]):-
+	string_concat('SELECT username FROM dbo.[User] WHERE idUser=', (H), Sql),
+	findall(X, odbc_query(wvm023, Sql, row(X)), [User|_]),
+	knowUser(H,User),
+	userByIdList(T).
 
 
-userTagsKnowledge(_,[]).
-userTagsKnowledge(U, [Tag|T]):-
+knowUser(UId,Username):-
+	assertz(user(Username)),
+	userTagIDs(UId,TagList),
+	knowUserTags(Username,TagList).
+
+
+knowUserTags(_,[]):-!.
+knowUserTags(U, [Tag|T]):-
 	assertz(user_tags(U,Tag)),
-	userTagsKnowledge(U,T).
+	knowUserTags(U,T).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
