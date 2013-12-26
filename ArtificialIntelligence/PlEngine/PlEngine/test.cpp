992b854385f1bec6d2a53689f4fbc8d1662e84c6
Fred Almeida <1101008@isep.ipp.pt>
2013-12-26 20:13:42 +0000

    [ALGAV] Template para comunicação C++->Prolog completo e pronto para adaptação. Já são retirados resultados de factos e de qualquer predicado Prolog.

--- ArtificialIntelligence/PlEngine/PlEngine/test.cpp
+++ ArtificialIntelligence/PlEngine/PlEngine/test.cpp
@@ -3,21 +3,83 @@
 
 using namespace std;
 
-int main(){
+//Degug->Project Proprieties->C/C++->Aditional include directories: .../swipl/include
+//Degug->Project Proprieties->Linker->Aditional library directories: .../swipl/lib
+//Degug->Project Proprieties->Linker->Input->Aditional Dependencies: libswipl.dll.a
+//Add ambient variable(PATH): .../swipl/bin
 
-	char* argv[] = {"swipl.ll","-s","C:\\Users\\Fred\\Desktop\\ISEP\\LAPR5\\Functions.pl",NULL};
+//example for adding numbers
+void sum(int x, int y)
+{
+	char* argv[] = {"swipl.ll","-s","C:\\Users\\W370ET\\Desktop\\ISEP\\LAPR5\\Lapr5Project\\ArtificialIntelligence\\Functions.pl",NULL};
 
+	PlEngine e(3,argv); 
+ 
+	PlTermv av(3);
+
+	av[0] = PlCompound((char*)x); 
+	av[1] = PlCompound((char*)y); 
+
+	PlQuery q("sum", av); 
+  
+	while(q.next_solution()){
+	
+		cout<<(char*)av[2]<<endl;
+	}
+}
+
+//example for getting fact solutions(1 argument)
+void fact(char* factname){
+
+	char* argv[] = {"swipl.ll","-s","C:\\Users\\W370ET\\Desktop\\ISEP\\LAPR5\\Lapr5Project\\ArtificialIntelligence\\Functions.pl",NULL};
+	
 	PlEngine e(3,argv);
 
 	PlTermv av(1);
 
-	PlQuery q("homem",av);
+	PlQuery q(factname,av);
 
 	while(q.next_solution()){
 	
 		cout<<(char*)av[0]<<endl;
 	}
 
+}
+
+
+int main(){
+
+	//sum(1,2);
+	
+	//fact("man");
+
 	cin.get();
-	return 1;
-	}
+	return 1;	
+}
+
+
+	/*PlEngine e(3,argv); 
+ 
+	PlTermv av(3);
+
+	av[0] = PlCompound("1"); 
+	av[1] = PlCompound("2"); 
+
+	PlQuery q("sum", av); 
+  
+	cout << (char*)av[2] << endl; 
+	*/
+
+
+	
+	/*PlEngine e(3,argv);
+
+	PlTermv av(2);
+
+	av[0] = PlCompound("peter"); 
+
+	PlQuery q("relationship",av);
+	
+	while(q.next_solution()){
+		cout<<(char*)av[1]<<endl;
+	}*/
