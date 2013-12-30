ee8ca457840b0b6d25bbe57547c17174c59e54bb
tjrocha27 <tiagor27@gmail.com>
2013-12-30 22:39:41 +0000

    [ALGAV]- encontrar amigos com X tags Feito

--- ArtificialIntelligence/IA prolog.pl
+++ ArtificialIntelligence/IA prolog.pl
@@ -28,7 +28,7 @@ tags(isep).
 tags(livros).
 
 tags_sub(fcp, fcporto).
-tags_sub(fcp, futebolClubePorto).
+tags_sub(fcp, futebolclubeporto).
 tags_sub(slb, slbenfica).
 tags_sub(slb, sportLisboaBenfica).
 tags_sub(scp, sportingcp).
@@ -66,37 +66,84 @@ relacao(luis,daniela).
 relacao(daniela,francisco).
 relacao(pedro,rocha).
 relacao(rocha,antonio).
-
+%------------------pesquisas para amigos------------------
 %amigos diretos de um utilizador
 amigos_diretos(Utilizador, L):-
 	findall(Y,(relacao(Utilizador,Y);relacao(Y,Utilizador)),L).
+
+
+amigo_Do_Amigo(Utilizador,Y,L,UtilizadorCentral):-
+	(relacao(Utilizador,Y);relacao(Y,Utilizador)), \+member(Y,L), Y \== UtilizadorCentral.
+
+%------------------pesquisas para tags--------------------
+
+%um utilizador
+tags_1_utilizador( Utilizador,L):-
+	findall(Tag,user_tags(Utilizador, Tag), L).
+
+%todos os nomes dessa tag
+encontra_tags_sub(H,LR):-
+	findall(Tag,(tags_sub(H,Tag);tags_sub(Tag,H)),L), append([H],L,LR).
+
+%Lista de utilizadores e as suas tags
+utilizadores_tags([],[]).
+utilizadores_tags([H|T],[[H|LTags]|LF]):-
+	tags_1_utilizador(H,LTags),utilizadores_tags(T,LF).
+
+% Dado as tags do amigo com os seus significados verifica se esta na
+% lista das tags do Utilizador
+
+
+
+verificaTagComum([H|_],LTagsUtilizador,Valor):-
+	member(H,LTagsUtilizador),Valor is 0+1.
+
+verificaTagComum([_|T],LTagsUtilizador,Valor):-
+	verificaTagComum(T,LTagsUtilizador,Valor).
+verificaTagComum([],_,_).
+
+
+%----------------------------------------------------------
+
 
 
 %tamanho da rede
 
 tamanho_rede_Utilizador(Utilizador):-
 	user(Utilizador),!,
-	amigos_diretos(Utilizador,L),tamanho_rede(L,L,_,Utilizador).
+	amigos_diretos(Utilizador,L),
+	tamanho_rede(L,L,_,Utilizador).
 
 tamanho_rede([],_,LF,UtilizadorCentral):-
 	length(LF,N), write('O Tamanho da rede do utilizador '),write(UtilizadorCentral),write(' é de '), write(N), write(' elementos.'), nl.
 
 tamanho_rede([H|T],L,_,UtilizadorCentral):-
-	findall(Y,amigo_Do_Amigo(H,Y,L,UtilizadorCentral),LR),append(L,LR,LX), tamanho_rede(T,LX,LX,UtilizadorCentral).
-
-amigo_Do_Amigo(Utilizador,Y,L,UtilizadorCentral):-
-	(relacao(Utilizador,Y);relacao(Y,Utilizador)), \+member(Y,L), Y \== UtilizadorCentral.
+	findall(Y,amigo_Do_Amigo(H,Y,L,UtilizadorCentral),LR),append(L,LR,LX),
+	tamanho_rede(T,LX,LX,UtilizadorCentral).
 
 
 %encontra amigos que tenham em comum X tags
 
-amigos_tags_comuns(Utilizador,L,LF):-
-	amigos_diretos(Utilizador,L),utilizador_tags(L,LF).
+obter_amigos_X_tags_comuns(Utilizador,X,ListaAmigos):-
+	amigos_diretos(Utilizador,LAmigosDirectos),
+	utilizadores_tags(LAmigosDirectos,LTagsAmigos),
+	obter_tags_comuns(LTagsAmigos,X,ListaAmigos,Utilizador,0).
 
-utilizador_tags([],[]).
+obter_tags_comuns([[Huser,Htag|TL]|T],X,ListaAmigos,Utilizador,N):-
+	encontra_tags_sub(Htag,LSignificadosTags),
+	tags_1_utilizador(Utilizador,LTagsUtilizador),
+	verificaTagComum(LSignificadosTags,LTagsUtilizador,Valor),Valor == 1, N1 is N + 1,
+	obter_tags_comuns([[Huser|TL]|T],X,ListaAmigos,Utilizador,N1);obter_tags_comuns([[Huser|TL]|T],X,ListaAmigos,Utilizador,N).
 
-utilizador_tags([H|T],[[H|LTags]|LF]):-
-	findall(Tag,user_tags(H,Tag),LTags),utilizador_tags(T,LF).
+obter_tags_comuns([[H|[]]|T],X,[H|ListaAmigos],Utilizador,N):-
+	N > 0, X = N, obter_tags_comuns(T,X,ListaAmigos,Utilizador,0).
+
+obter_tags_comuns([[_|[]]|T],X,ListaAmigos,Utilizador,0):-
+	obter_tags_comuns(T,X,ListaAmigos,Utilizador,0).
+
+obter_tags_comuns([],_,_,_,_).
+
+
 
 
 
