0b5b14d794072a1aff3c2fd2a07b27bfc5428442
d4rc92 <danibento619@gmail.com>
2013-12-30 18:24:47 +0000

    [ARQSI] Webservice update.
    Merge all changes

---
+++ WebSite/GameWS/GameWebService.svc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using DataModel.Tools;
+using DataModel.BLL;
+using DataModel.Model;
+
+namespace GameWS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class GameWebService : IGameWebService
+    {
+
+        /// <summary>
+        /// Metodo para validar o login. Retorna o id do utilizador caso seja sucesso, ou caso de insucesso retorna (-1)
+        /// </summary>
+        /// <param name="username">Username do utilizador</param>
+        /// <param name="password">Palavra passe sem qualquer encriptacao</param>
+        /// <param name="securityPass">Key confidencial do WS, para evitar acessos indesejados</param>
+        /// <returns>Id do utilizador ou (-1) em caso de insucesso</returns>
+        public int login(string username, string password, string securityPass)
+        {
+
+            if (!validateSecurityPass(securityPass))
+            {
+                return -1;
+            }
+
+            UserBLL userBll = new UserBLL();
+
+            return userBll.verifyAutenticationUser(username, password);
+        }
+
+        private bool validateSecurityPass(string securityPass)
+        {
+            if (securityPass == "ThinkCode")
+                return true;
+            return false;
+
+        }
+
+        private bool checkUserIsInList(int idUser, List<IUser> ul)
+        {
+            for (int i = 0; i < ul.Count; i++)
+            {
+                if (ul[i].IdUser == idUser)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void addUserToReturnList(User u, List<IUser> ul)
+        {
+            if(!(checkUserIsInList(u.IdUser, ul))){
+                IUser tmp = new IUser();
+                tmp.IdUser= u.IdUser;
+                tmp.Username = u.Username;
+                tmp.Name = u.Name;
+                tmp.Points = u.Points;
+                
+                for(int i=0; i<u.UserTags.Count; i++){
+                    tmp.UserTags.Add(u.UserTags[i].TagName);
+                }
+
+                for(int i=0; i<u.FriendshipList.Count; i++){
+                    Tuple<int,string> t = new Tuple<int,string> ((int) u.FriendshipList[i].Item1, (string) u.FriendshipList[i].Item3.TagName);      
+                    tmp.FriendshipList.Add(t);
+                }
+
+                for(int i=0; i<u.FriendshipRequestList.Count; i++){
+                    tmp.FriendshipRequestList.Add(u.FriendshipRequestList[i].Item1);
+                }
+
+                for(int i=0; i<u.RelationshipRequestNegociation.Count; i++){
+                    for(int j=0; j<u.RelationshipRequestNegociation[i].Item2.Count; j++){
+                        Tuple<int,int,int,bool> t = new Tuple<int,int,int,bool> ((int)u.RelationshipRequestNegociation[i].Item1,(int)u.RelationshipRequestNegociation[i].Item2[j].IdGame,(int)u.RelationshipRequestNegociation[i].Item2[j].Difficulty,(bool) u.RelationshipRequestNegociation[i].Item2[j].Status);
+                        tmp.RelationshipRequestNegociation.Add(t);
+                    }                                       
+                }
+
+            }
+        }
+
+        public List<IUser> loadUserNetwork(int idUser)
+        {
+            List<IUser> userList = new List<IUser>();
+
+            UserBLL bll = new UserBLL();
+
+            User u = bll.loadUserById(idUser);
+
+
+            return userList;
+        }
+
+       
+    }
+}
---
+++ WebSite/GameWS/IGameWebService.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace GameWS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IGameWebService
+    {
+
+        [OperationContract]
+        int login(string username, string password, string securityPass);
+
+        [OperationContract]
+        List<IUser> loadUserNetwork(int idUser);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    [DataContract]
+    public class IUser
+    {
+        private int _idUser;
+
+        [DataMember]
+        public int IdUser
+        {
+            get { return _idUser; }
+            set { _idUser = value; }
+        }
+
+        private string _username;
+
+        [DataMember]
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        private string _name;
+
+        [DataMember]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _points;
+
+        [DataMember]
+        public int Points
+        {
+            get { return _points; }
+            set { _points = value; }
+        }
+
+        IList<string> _userTags;
+
+        [DataMember]
+        public IList<String> UserTags
+        {
+            get { return _userTags; }
+            set { _userTags = value; }
+        }
+
+        
+        public void addTagToUser(string tag){
+            _userTags.Add(tag);
+        }
+
+        
+        public void remTagFromUser(string tag) {
+            _userTags.Remove(tag);
+        }
+        
+        private IList<Tuple<int, string>> _friendshipList;
+
+        [DataMember]
+        public IList<Tuple<int, string>> FriendshipList
+        {
+            get { return _friendshipList; }
+            set { _friendshipList = value; }
+        }
+
+        
+        public void addRelationship(Tuple<int, string> user)
+        {
+            _friendshipList.Add(user);
+        }
+
+        
+        public void remRelationship(Tuple<int, string> user)
+        {
+            _friendshipList.Remove(user);
+        }
+
+        
+        private IList<int> _friendshipRequestList;
+        
+        [DataMember]
+        public IList<int> FriendshipRequestList
+        {
+            get { return _friendshipRequestList; }
+            set { _friendshipRequestList = value; }
+        }
+
+        
+        public void addFriendshipRequest(int user)
+        {
+            _friendshipRequestList.Add(user);
+        }
+
+        
+        public void remFriendshipRequest(int user)
+        {
+            _friendshipRequestList.Remove(user);
+        }
+
+        private IList<Tuple<int, int, int, bool>> _relationshipRequestNegociation;
+
+        [DataMember]
+        public IList<Tuple<int, int, int, bool>> RelationshipRequestNegociation
+        {
+            get { return _relationshipRequestNegociation; }
+            set { _relationshipRequestNegociation = value; }
+        }
+
+        
+        public void addRelationshipRequestNegociation(Tuple<int, int, int, bool> user)
+        {
+            _relationshipRequestNegociation.Add(user);
+        }
+
+        
+        public void remRelationshipRequest(Tuple<int, int, int, bool> user)
+        {
+            _relationshipRequestNegociation.Remove(user);
+        }
+
+        
+        public IUser()
+        {
+            _userTags = new List<string>();
+        }
+
+    }
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    //[DataContract]
+    //public class CompositeType
+    //{
+    //    bool boolValue = true;
+    //    string stringValue = "Hello ";
+
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
+}
