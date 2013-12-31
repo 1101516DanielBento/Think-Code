ee984be1a53433a5227baae20a5581afe9e9b568
Luis Marques <1080702@isep.ipp.pt>
2013-12-31 17:04:15 +0000

    [ARQSI] - Pagina que faz o pedido de negiacao por parte do utilizador. Ainda incompleta.

---
+++ WebSite/SocialGameWebSite/ChallengingPlayer.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataModel.BLL;
+using DataModel.Model;
+
+public partial class ChallengingPlayer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (Session["id"] == null)
+        //    Response.Redirect("Login.aspx");
+        
+        int idUser = 0;
+
+        if (Request.QueryString["usr"] != null)
+            idUser = Convert.ToInt32(Request.QueryString["usr"]);
+
+        if (idUser != 0)
+        {
+            int tipoJogo = 0;
+            int grauDificuldade = 0;
+
+
+            IList<Tuple<int, int>> negociacao;
+
+
+           // negociacao.Add(new Tuple<int,int>(1,1)); // erro 
+
+
+            UserBLL usrBll = new UserBLL();
+            bool success = usrBll.negociateFriendshipRequest((int)Session["id"], idUser,negociacao);
+
+
+            if (success)
+                Response.Redirect("PendingRequest.aspx?request=1&usr=" + idUser.ToString()); //
+        }
+
+
+        Response.Redirect("ShowAllUsers.aspx");
+    }
+    }
+}
