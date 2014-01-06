12b29ce69ac5e68b1e737b55d8e80a1f35afb3a6
1100655 <1100655@isep.ipp.pt>
2014-01-06 10:23:13 +0000

    [SGRAI] - Merge

--- Game3D/Lapr5/Lapr5/Labirinto.cpp
+++ Game3D/Lapr5/Lapr5/Labirinto.cpp
@@ -4,12 +4,13 @@
 //
 //  Created by pedrosimoes on 22/12/13.
 //  Copyright (c) 2013 pedrosimoes. All rights reserved.
-
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <vector>
+#include <iostream>
 
 
 #ifdef __APPLE__
@@ -62,7 +63,7 @@ void drawMaze(vector<vector<Cell>>  maze)
     }
 }
 
-void PrintMaze(){
+void PrintMaze(vector<vector<Cell>>  maze){
     for(int i = 0; i < 2*10; ++i){
         cout << "_";
     }
@@ -79,7 +80,7 @@ void PrintMaze(){
         cout << endl;
     }
 }
-
+*/
 /*int main()
 {
 	return 0;
