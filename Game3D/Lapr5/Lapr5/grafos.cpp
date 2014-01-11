b79e4874b0c204a85235f188c0a693c635d32c89
1100655 <1100655@isep.ipp.pt>
2014-01-11 17:56:59 +0000

    [SGRAI] update ficheiro grafos.cpp

--- Game3D/Lapr5/Lapr5/grafos.cpp
+++ Game3D/Lapr5/Lapr5/grafos.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 
 #ifdef __APPLE__
-#define __GRAFO__FILE__ "/Users/pedrosimoes/Think-Code/Game3D/Lapr5/Lapr5/exemplo.grafo"
+#define __GRAFO__FILE__ "/Users/simoes/Documents/Bitbucket/LAPR5/Game3D/Lapr5/Lapr5/a.grafo"
 #else
 #define __GRAFO__FILE__ "exemplo.grafo"
 #endif
