4e957eb24388579fb0db8b0c31ec2ccdf6964aab
d4rc92 <danibento619@gmail.com>
2014-01-11 18:06:37 +0000

    [SGRAI] Implementacao das modificacoes do Webservice na classe que conecta o jogo ao WebService

--- Game3D/Lapr5/Lapr5/WebService_Request.cpp
+++ Game3D/Lapr5/Lapr5/WebService_Request.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 bool WebService_Request::to_bool(std::string const& s) {
-     return s != "0" || s !="False";
+
+
+	if(s == "1")
+		return true;
+	if(s =="True")
+		return true;
+
+	return false;
 }
 
 vector<string> WebService_Request::splitString(string s, string delimiter)
@@ -37,9 +44,9 @@ User_C WebService_Request::convertObjWStoUser(UserType *usr){
 	u->setUsername(convertWcharToString(usr->Username));
 	u->setPoints(usr->Points);
 	u->setMoodState(usr->MoodState);
-	
+
 	vector<string>::iterator it;
-	
+
 	//TAG LIST
 	vector<string> tagTmp = splitString(convertWcharToString(usr->UserTags),";");
 	int i=0;
@@ -51,10 +58,10 @@ User_C WebService_Request::convertObjWStoUser(UserType *usr){
 		string tgN=tagTmp2[1];
 		tuple<int,string> tup(idT,tgN);
 		tagListUsr->push_back(tup);
-            
-    }
+
+	}
 	u->setTagList(tagListUsr);
-	
+
 	//FRIENDS
 	vector<tuple<int,string>> *friends = new vector<tuple<int,string>>();
 	vector<string> frTMP = splitString(convertWcharToString(usr->FriendshipList),";");
@@ -67,7 +74,7 @@ User_C WebService_Request::convertObjWStoUser(UserType *usr){
 		tuple<int,string> tup(idT,tgN);
 		friends->push_back(tup);
 
-    }
+	}
 	u->setFriends(friends);
 
 	//REQUESTS
@@ -79,7 +86,7 @@ User_C WebService_Request::convertObjWStoUser(UserType *usr){
 		int nr= atoi(rTMP[i].c_str());
 		req->push_back(nr);
 
-    }
+	}
 	u->setRequest(req);
 
 	vector<tuple<int,vector<tuple<int,int,bool>>>> *negs = new vector<tuple<int,vector<tuple<int,int,bool>>>>();
@@ -89,24 +96,27 @@ User_C WebService_Request::convertObjWStoUser(UserType *usr){
 
 		vector<string> tagTmp2 = splitString(*it,":");
 		int idT= atoi(tagTmp2[0].c_str());
-		
+
 		vector<string> tmp3 = splitString(*it,"->");
 		vector<string>::iterator it2;
 		int i2=0;
-
+		bool flag=false;
 		vector<tuple<int,int,bool>> vecT;
-		for(it2=negL.begin() ; it2 < negL.end(); it2++,i2++ ) {
-			vector<string> tagTmp2 = splitString(*it,",");
-			int idG= atoi(tagTmp2[0].c_str());
-			int dif=atoi(tagTmp2[1].c_str());
-			bool st = to_bool(tagTmp2[2].c_str());
-			tuple<int,int,bool> tup(idG,dif,st);
-			vecT.push_back(tup);
+		for(it2=tmp3.begin() ; it2 < tmp3.end(); it2++,i2++ ) {
+			if(flag){
+				vector<string> tagTmp2x = splitString(*it2,",");
+				int idG= atoi(tagTmp2x[0].c_str());
+				int dif=atoi(tagTmp2x[1].c_str());
+				bool st = to_bool(tagTmp2x[2].c_str());
+				tuple<int,int,bool> tup(idG,dif,st);
+				vecT.push_back(tup);
+			}
+			flag=true;
 		}
 		tuple<int,vector<tuple<int,int,bool>>> tpFN(idT,vecT);
 		negs->push_back(tpFN);
 
-    }
+	}
 	u->setReqNeg(negs);
 
 
@@ -158,7 +168,7 @@ vector<User_C> * WebService_Request::getNetworkById(int idUser){
 
 
 	hr=BasicHttpBinding_IGameWebService_loadUserNetwork(proxy,idUser,key,&listSize,&user,heap,NULL,0,NULL,error);
-	
+
 	return convertWebServiceListToVector(user,listSize);
 
 }
@@ -175,12 +185,68 @@ int WebService_Request::login(string UserName, string Password){
 		char act = (char) Password[i];
 		password[i]=act;
 	}
-		
+
 	WCHAR key[] = L"ThinkCode";
 	int result = -2;
 
-	
+
 	hr= BasicHttpBinding_IGameWebService_login(proxy,username,password,key, &result, heap, NULL, 0, NULL, error);
-	
+
 	return result;
+}
+
+
+bool WebService_Request::changeUserPoints(int id, int pointsToAdd){
+	WCHAR key[] = L"ThinkCode";
+	BOOL result=false;
+	hr= BasicHttpBinding_IGameWebService_changeUserPoints(proxy,id,pointsToAdd,key, &result, heap, NULL, 0, NULL, error);
+
+	return result;
+}
+bool WebService_Request::changeMoodState(int idUser, int mood){
+	WCHAR key[] = L"ThinkCode";
+	BOOL result=false;
+	hr= BasicHttpBinding_IGameWebService_changeMoodState(proxy,idUser,mood,key, &result, heap, NULL, 0, NULL, error);
+
+	return result;
+
+}
+
+User_C WebService_Request::doNegociationGameComplete(int userId_ONPLAY, int UserIdB, int idGame, int difficulty){
+
+	WCHAR key[] = L"ThinkCode";
+	UserType **u;
+	unsigned int listSize=0;
+	u=(UserType**)malloc((sizeof (UserType))*listSize);
+	hr= BasicHttpBinding_IGameWebService_doNegociationGameComplete(proxy,userId_ONPLAY,UserIdB,idGame, difficulty,key,&listSize,&u, heap, NULL, 0, NULL, error);
+
+	return convertObjWStoUser(u[0]);
+
+}
+
+vector<Dictionary> *WebService_Request::getDictionarys(){
+	WCHAR key[] = L"ThinkCode";
+	Dict **d;
+	unsigned int listSize=0;
+	d=(Dict**)malloc((sizeof (Dict))*listSize+1);
+	hr= BasicHttpBinding_IGameWebService_getDictionarys(proxy,key,&listSize,&d, heap, NULL, 0, NULL, error);
+
+	vector<Dictionary> *ret = new vector<Dictionary>();
+	for(int i=0; i<listSize; i++){
+		Dictionary *tmp = new Dictionary();
+		tmp->setIdWord(d[i]->IdDict);
+		tmp->setWordName(convertWcharToString(d[i]->Word));
+
+		vector<string> tgTmp= splitString(convertWcharToString(d[i]->TagsUsed),";");
+		vector<int> *t=new vector<int>();
+		for(int j=0;j<tgTmp.size();j++){
+			t->push_back(atoi(tgTmp[i].c_str()));
+		}
+		tmp->setTagList(t);
+
+		ret->push_back(*tmp);
+	}
+
+
+	return ret;
 }
