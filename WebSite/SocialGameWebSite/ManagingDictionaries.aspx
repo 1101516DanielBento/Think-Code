c558b9da9f371b0b8605286ab217573dd14df801
Luis Marques <1080702@isep.ipp.pt>
2014-01-12 17:21:21 +0000

    [ARQSI] - Implementada pagina de administração de Dicionarios ao qual só um utilizador com permissões tem acesso.

---
+++ WebSite/SocialGameWebSite/ManagingDictionaries.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FrontOffice.master" AutoEventWireup="true" CodeFile="ManagingDictionaries.aspx.cs" Inherits="ManagingDictionaries" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="UserLoginContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="LayoutButtonsContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="MainContent" Runat="Server">
+    <p class="MsoNormal">
+        &nbsp;</p>
+    <span style="font-size:12.0pt;line-height:107%;color:#548235;
+mso-themecolor:accent6;mso-themeshade:191;mso-style-textfill-fill-color:#548235;
+mso-style-textfill-fill-themecolor:accent6;mso-style-textfill-fill-alpha:100.0%;
+mso-style-textfill-fill-colortransforms:lumm=75000">&nbsp;To add a tag to a dictionary just enter your IDs in the boxes below.<br />
+    <br />
+&nbsp; Move Tag&nbsp;
+    <asp:TextBox ID="IdTagBox" runat="server" Width="39px"></asp:TextBox>
+&nbsp;</span>&nbsp;&nbsp; <span style="font-size:12.0pt;line-height:107%;color:#548235;
+mso-themecolor:accent6;mso-themeshade:191;mso-style-textfill-fill-color:#548235;
+mso-style-textfill-fill-themecolor:accent6;mso-style-textfill-fill-alpha:100.0%;
+mso-style-textfill-fill-colortransforms:lumm=75000">to a Dictionary
+    <asp:TextBox ID="IdDicBox" runat="server" Width="39px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btInserirTagToDic" runat="server" OnClick="btInserirTagToDic_Click" Text="Insert" />
+&nbsp;&nbsp;&nbsp; </span>&nbsp;<asp:Label ID="lblMensagem" runat="server" Font-Bold="True" CssClass="p_sucesso"></asp:Label>                
+    &nbsp;<asp:Label ID="lblMsgErro" runat="server" Font-Bold="True" CssClass="p_erro"></asp:Label>                
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <div style="float: left; width: 400px;">
+    <asp:GridView ID="GridView1" cellpadding="20" cellspacing="5"  runat="server" AutoGenerateColumns="False" 
+        OnPageIndexChanging="GridView1_PageIndexChanging" AllowPaging="True" PageSize="5">
+        <Columns>
+            <asp:BoundField DataField="NomeDicionario" DataFormatString="{0}" HeaderText="Nome Dicionario" />
+            <asp:BoundField DataField="IdDic" DataFormatString="{0}" HeaderText="ID" />
+        </Columns>
+    </asp:GridView>
+        </div>
+
+    <div style="float: left; width: 400px;">
+    <asp:GridView ID="GridView2" cellpadding="20" cellspacing="5"  runat="server" AutoGenerateColumns="False" 
+        OnPageIndexChanging="GridView2_PageIndexChanging" AllowPaging="True" OnSelectedIndexChanged="GridView2_SelectedIndexChanged" PageSize="8">
+        <Columns>
+            <asp:BoundField DataField="NomeTag" DataFormatString="{0}" HeaderText="Tag Name" />
+            <asp:BoundField DataField="IdTag" DataFormatString="{0}" HeaderText="ID Tag" />
+            <asp:BoundField DataField="DicionarioTag" DataFormatString="{0}" HeaderText="Dictionary" />
+        </Columns>
+    </asp:GridView>
+        </div>
+
+    <br />
+    <br />
+    <br />
+    </asp:Content>
+
---
+++ WebSite/SocialGameWebSite/ManagingDictionaries.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Data;
+using DataModel.BLL;
+using DataModel.Model;
+
+public partial class ManagingDictionaries : System.Web.UI.Page
+{
+    int requestOk = -1;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["id"] == null)
+            Response.Redirect("Login.aspx");
+
+        BindGridView1(); //tags
+        BindGridView2(); //dicionarios
+
+        if (Request.QueryString["requestOk"] != null)
+            requestOk = Convert.ToInt32(Request.QueryString["requestOk"]);
+
+        if (requestOk == 0) //Erro 
+            lblMsgErro.Text = "Error! You must enter ID in the two text boxes!";
+
+        if (requestOk == 1) //Erro
+            lblMensagem.Text = "Error! You must enter positive numbers!";
+
+        if (requestOk == 2) //Erro
+            lblMensagem.Text = "Tag moved successfully!";
+
+        if (requestOk == 3) //Erro
+            lblMensagem.Text = "We could not put the tag! Problems in the database";
+
+
+    }
+
+    private void BindGridView2() //mostra Tags
+    {
+
+        DataTable dataTable = new DataTable();
+        dataTable.Columns.Add("NomeTag");
+        dataTable.Columns.Add("IdTag");
+        dataTable.Columns.Add("DicionarioTag");
+        dataTable.Columns.Add("Link");
+        dataTable.Columns.Add("Addlink");
+        dataTable.Columns.Add("IdUser");
+
+        TagBLL tagBll = new TagBLL();
+        IList<Tag> listaTags = new List<Tag> { };
+
+        IList<Dictionary> listaDicionarios = new List<Dictionary>();
+
+        listaDicionarios = tagBll.loadAllDictionarys(); //retorna lista de dicionarios
+
+        listaTags = tagBll.getAllUserTags();
+
+        IList<int> listaTagsToDic = new List<int>();
+
+        foreach (Tag tag in listaTags)
+        {
+            DataRow dr = dataTable.NewRow();
+
+            dr["NomeTag"] = tag.TagName;
+            dr["IdTag"] = tag.IdTag;
+
+            foreach (Dictionary dic in listaDicionarios)
+            {
+               listaTagsToDic = dic.ListTagsWord;
+               foreach (int idTag in listaTagsToDic)  
+               {
+                   if (idTag == tag.IdTag)
+                   {
+                       if((dic.Word.Equals("Null")))
+                           dr["DicionarioTag"] = "-------";
+                       else
+                           dr["DicionarioTag"] = dic.Word;
+
+                   }
+   
+               }
+                
+            }
+
+            
+
+            dataTable.Rows.Add(dr);
+        }
+
+        GridView2.DataSource = dataTable;
+        GridView2.DataBind();
+        GridView2.Visible = true;
+ }
+
+    private void BindGridView1() //Dicionarios
+    {
+        TagBLL tagBll = new TagBLL();
+
+        IList<Dictionary> listaDicionarios = new List<Dictionary>();
+
+        listaDicionarios = tagBll.loadAllDictionarys(); //retorna lista de dicionarios
+
+        
+        DataTable dataTable = new DataTable();
+        dataTable.Columns.Add("NomeDicionario");
+        //dataTable.Columns.Add("AdicionaTag");
+        dataTable.Columns.Add("IdMood");
+        dataTable.Columns.Add("Addlink");
+        dataTable.Columns.Add("IdUser");
+        dataTable.Columns.Add("IdDic");
+        
+        foreach (Dictionary dic in listaDicionarios)
+        {
+            DataRow dr = dataTable.NewRow();
+
+            dr["NomeDicionario"] = dic.Word;
+            //dr["AdicionaTag"] = "Add Tag to Dictionary";
+            dr["IdDic"] = dic.IdWord;
+
+            dataTable.Rows.Add(dr);
+        }
+
+        GridView1.DataSource = dataTable;
+        GridView1.DataBind();
+        GridView1.Visible = true;
+    }
+
+
+
+    protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void GridView1_PageIndexChanging(Object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        BindGridView1();
+    }
+
+    protected void GridView2_PageIndexChanging(Object sender, GridViewPageEventArgs e)
+    {
+        GridView2.PageIndex = e.NewPageIndex;
+        BindGridView2();
+    }
+
+    protected void btInserirTagToDic_Click(object sender, EventArgs e)
+    {
+        if (IdTagBox.Text.Equals("") || IdDicBox.Text.Equals(""))
+        {
+            Response.Redirect("ManagingDictionaries.aspx?requestOk=0");
+        }
+        else
+            if (Convert.ToInt32(IdTagBox.Text) < 0 || Convert.ToInt32(IdDicBox.Text) < 0)
+                Response.Redirect("ManagingDictionaries.aspx?requestOk=1");
+            else
+            {
+                int idTag = Convert.ToInt32(IdTagBox.Text);
+                int idDic = Convert.ToInt32(IdDicBox.Text);
+
+
+                TagBLL tagBll = new TagBLL();
+
+                tagBll.changeTagToDictionary(idDic, idTag);
+                    
+                    //addNewWordDictionary(idTag); //este cria dicionario
+
+                Response.Redirect("ManagingDictionaries.aspx?requestOk=2");
+
+            }
+
+        Response.Redirect("ManagingDictionaries.aspx?requestOk=3");
+
+    }
+}
